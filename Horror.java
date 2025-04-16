7842c1c8d45159672ca5c416e885ff14e6be4d35
Kudzani46 <twitymalik1@gmail.com>
2025-04-16 19:39:58 +0200

    Update Horror.java

--- Horror.java
+++ Horror.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Horror extends Movie{
-    protected String scaryCreature;
+    private String scaryCreature;
 
     public Horror(ArrayList<Cast> cast, ArrayList<Crew> crew, ArrayList<String> genres, ArrayList<String> languages, String title,String releaseDate, String description, int duration,String scaryCrreature){
       super(cast, crew, genres, languages, title, releaseDate, description, duration);
